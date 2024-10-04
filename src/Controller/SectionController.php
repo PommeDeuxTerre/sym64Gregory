384bd20306ca47b96f6cb4d0cf0792ea1420ec0e
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-10-04 12:09:48 +0200

    sections page

--- src/Controller/SectionController.php
+++ src/Controller/SectionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,22 +10,26 @@ use Symfony\Component\Routing\Attribute\Route;
 class SectionController extends AbstractController
 {
     #[Route('/sections', name: 'sections')]
-    public function index(): Response
+    public function index(SectionRepository $SectionRepository): Response
     {
         $user = $this->getUser();
+        $sections = $SectionRepository->findAll();
         return $this->render('section/index.html.twig', [
             'controller_name' => 'SectionController',
             'user' => $user,
+            'sections' => $sections,
         ]);
     }
 
     #[Route('/section/{id}', name: 'section')]
-    public function section(): Response
+    public function section(int $id, SectionRepository $SectionRepository): Response
     {
         $user = $this->getUser();
+        $section = $SectionRepository->find($id);
         return $this->render('section/index.html.twig', [
             'controller_name' => 'SectionController',
             'user' => $user,
+            'section' => $section,
         ]);
     }
 }
--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,22 +10,26 @@ use Symfony\Component\Routing\Attribute\Route;
 class TagController extends AbstractController
 {
     #[Route('/tags', name: 'tags')]
-    public function index(): Response
+    public function index(TagRepository $TagRepository): Response
     {
         $user = $this->getUser();
+        $tags = $TagRepository->findAll();
         return $this->render('tag/index.html.twig', [
             'controller_name' => 'TagController',
             'user' => $user,
+            'tags' => $tags,
         ]);
     }
 
     #[Route('/tag/{id}', name: 'tag')]
-    public function tag(int $id): Response
+    public function tag(int $id, TagRepository $TagRepository): Response
     {
         $user = $this->getUser();
+        $tag = $TagRepository->find($id);
         return $this->render('tag/index.html.twig', [
             'controller_name' => 'TagController',
             'user' => $user,
+            'tag' => $tag,
         ]);
     }
 }
