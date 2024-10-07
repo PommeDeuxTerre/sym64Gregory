dcc967b1605f9a65ea6c65ff1a4ac48454514450
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-10-07 16:07:06 +0200

    make the comment form prettier

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -3,9 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Comment;
-use App\Entity\Post;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +12,7 @@ class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('commentMessage')
-        ;
+        $builder->add('commentMessage', TextareaType::class, ['attr' => ['placeholder' => 'Votre Message', 'rows' => 5, 'cols' => 40]]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
