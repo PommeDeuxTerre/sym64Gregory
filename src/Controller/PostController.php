a8a8633c1e7510a80731fe74b6a45966ab3fcaa2
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-10-07 15:44:36 +0200

    show comments

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Comment;
 use App\Form\CommentType;
+use App\Repository\CommentRepository;
 use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,7 +15,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class PostController extends AbstractController
 {
     #[Route('/post/{id}', name: 'app_post')]
-    public function index(Request $request, int $id, PostRepository $PostRepository, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, int $id, PostRepository $PostRepository, EntityManagerInterface $entityManager, CommentRepository $CommentRepository): Response
     {
         $user = $this->getUser();
         $post = $PostRepository->getPostById($id);
@@ -22,6 +23,10 @@ class PostController extends AbstractController
         $comment = new Comment();
         $form = $this->createForm(CommentType::class, $comment);
         $form->handleRequest($request);
+
+        $comments = $CommentRepository->findAll();
+        // filter out the comments of other posts (yes I should have done that in the up line)
+        $comments = array_filter($comments, fn($comment) => $comment->getPost() == $post);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $comment->setUser($this->getUser());
@@ -33,6 +38,7 @@ class PostController extends AbstractController
         return $this->render('post/index.html.twig', [
             'controller_name' => 'PostController',
             'post' => $post,
+            'comments' => $comments,
             'form' => $form,
             'user' => $user,
         ]);
