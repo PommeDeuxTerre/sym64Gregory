941cd03b02f67018a9866c409bed87972adb1b70
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-10-09 11:07:59 +0200

    crud section create

--- src/Form/SectionType.php
+++ src/Form/SectionType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Section;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,7 @@ class SectionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('sectionTitle')
+            ->add('sectionTitle', TextType::class, ['attr' => ['placeholder' => 'Nom de la Section']])
             ->add('sectionDescription', TextareaType::class, ['attr' => ['placeholder' => 'Votre Description', 'rows' => 5, 'cols' => 40]])
         ;
     }
