a7225d305dccb485a2b14d421606c2674bcfa398
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-10-09 11:22:46 +0200

    tag crud form

--- src/Form/TagType.php
+++ src/Form/TagType.php
@@ -2,10 +2,9 @@
 
 namespace App\Form;
 
-use App\Entity\Post;
 use App\Entity\Tag;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,14 +12,7 @@ class TagType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('tagName')
-            ->add('posts', EntityType::class, [
-                'class' => Post::class,
-                'choice_label' => 'id',
-                'multiple' => true,
-            ])
-        ;
+        $builder->add('tagName', TextType::class, ['attr' => ['placeholder' => 'Nom du Tag']]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
