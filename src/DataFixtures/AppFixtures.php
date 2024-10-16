ee17a4c09ae3e597a824bc8a50ba8f17552d7798
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-10-16 11:17:24 +0200

    fixture tag and section

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,8 @@ namespace App\DataFixtures;
 
 use App\Entity\Comment;
 use App\Entity\Post;
+use App\Entity\Section;
+use App\Entity\Tag;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -42,6 +44,24 @@ class AppFixtures extends Fixture
             $manager->persist($user);
         }
 
+        // tags
+        for ($i=1;$i<=20;$i++){
+            $tag = new Tag();
+            $tag->setTagName(ucfirst($faker->shuffle($faker->word()).$faker->shuffle($faker->word()).$faker->shuffle($faker->word())));
+            $tags[] = $tag;
+            $manager->persist($tag);
+        }
+
+        // sections
+        for ($i=1;$i<=20;$i++){
+            $section = new Section();
+
+            $section->setSectionTitle(ucfirst($faker->words(rand(1, 2),true)));
+            $section->setSectionDescription($faker->paragraphs(1,true));
+            $sections[] = $section;
+            $manager->persist($section);
+        }
+
         // posts
         for ($i=1;$i<=100;$i++){
             $post = new Post();
@@ -56,17 +76,31 @@ class AppFixtures extends Fixture
             $post->setPostTitle(ucfirst($faker->words(mt_rand(2,5),true)));
             $post->setPostDescription($faker->paragraphs(mt_rand(3,6), true));
 
+
+            // tags
+            for ($j=1;$j<=5;$j++){
+                $rand_tag = $tags[array_rand($tags)];
+                $post->addTag($rand_tag);
+            }
+
+            // sections
+            for ($j=1;$j<=5;$j++){
+                $rand_section = $sections[array_rand($sections)];
+                $post->addSection($rand_section);
+            }
+
             $posts[] = $post;
 
             $manager->persist($post);
         }
-        $published_post = array_values(array_filter($posts, fn(Post $post)=>$post->isPostPublished()));
+
+        $published_posts = array_values(array_filter($posts, fn(Post $post)=>$post->isPostPublished()));
 
         // comments
-        for ($i=1;$i<=sizeof($published_post)*5;$i++){
+        for ($i=1;$i<=sizeof($published_posts)*5;$i++){
             $comment = new Comment();
             $rand_user = $users[array_rand($users)];
-            $rand_post = $published_post[array_rand($published_post)];
+            $rand_post = $published_posts[array_rand($published_posts)];
 
             $comment->setUser($rand_user);
             $comment->setPost($rand_post);
