1288ec5f864be4be9c37d36087fda68de1b783b1
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-10-29 09:53:23 +0100

    crud permission

--- src/Controller/CrudController.php
+++ src/Controller/CrudController.php
@@ -14,12 +14,24 @@ class CrudController extends AbstractController
     {
         $user = $this->getUser();
         if (!$user || !in_array("ROLE_ADMIN", $user->getRoles()) && !in_array("ROLE_REDAC", $user->getRoles()))return $this->redirectToRoute('app_login');
+        if (in_array("ROLE_ADMIN", $user->getRoles())){
+            $cruds = [
+                ["app_comment_index", "Commentaires Crud"],
+                ["app_crud_article_index", "Articles Crud"],
+                ["app_crud_section_index", "Sections Crud"],
+            ];
+        }else {
+            $cruds = [
+                ["app_crud_article_index", "Articles Crud"],
+            ];
+        }
 
         $sections = $SectionRepository->findAll();
         return $this->render('crud/index.html.twig', [
             'controller_name' => 'CrudController',
             'user' => $user,
             'sections' => $sections,
+            'cruds' => $cruds,
         ]);
     }
 }
